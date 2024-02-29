9aa42a8681d45f84f4e52d5a19d1752754278e79
Rafael Simoes <r4faelsimoes@gmail.com>
2024-02-29 18:39:29 +1100

    Handle missing parameters gracefully

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  rescue_from ActionController::ParameterMissing do |exception|
+    missing_param = exception.param.to_s
+    render json: { errors: { missing_param => 'missing' } }, status: :bad_request
+  end
 end
